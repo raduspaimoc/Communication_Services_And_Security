316f10aeebff4ae3329123dbc3ab78f5fa1540ff
raduspaimoc <raduspaimoc@gmail.com>
2021-04-10 12:28:55 +0200

    LAB 3. Problem 2 completed.

--- Lab 3/Problem 2/sript.sh
+++ Lab 3/Problem 2/sript.sh
@@ -17,12 +17,12 @@ bytes1=0
 bytes2=0
 
 
-bytes_array=$(tshark -Y "ip.src ==11.0.0.1" -r captureP2.pcapng -T fields -e frame.len)
+bytes_array=$(tshark -Y "ip.src ==11.0.0.1" -r Capture.pcapng -T fields -e frame.len)
 get_total_bytes "${bytes_array[@]}"
 bytes1=$bytes
 total_bytes=$bytes
 
-bytes_array=$(tshark -Y "ip.src ==12.0.0.1" -r captureP2.pcapng -T fields -e frame.len)
+bytes_array=$(tshark -Y "ip.src ==12.0.0.1" -r Capture.pcapng -T fields -e frame.len)
 get_total_bytes "${bytes_array[@]}"
 bytes2=$bytes
 
@@ -34,9 +34,9 @@ echo "-------------------------------"
 echo "Total bytes transfered: $total_bytes Bytes"
 echo "-------------------------------"
 echo "TAP 0: $bytes1 Bytes"
-echo "Bandwidth occupation TAP 0: $bo_1%"
+echo "TAP 0 - Bandwidth occupation: $bo_1%"
 echo "-------------------------------"
 echo "TAP 1: $bytes2 Bytes"
-echo "Bandwidth occupation TAP 1: $bo_2%"
+echo "TAP 1 - Bandwidth occupation: $bo_2%"
 echo "-------------------------------"
 
